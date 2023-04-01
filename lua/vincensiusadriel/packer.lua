0734b5b52e03938cf4435beadaabf5a0958140c5
“vincensiusadrieltoped” <vincensius.adriel@tokopedia.com>
2023-04-01 21:56:09 +0700

    remove toggleterm (replaced by tmux)

--- lua/vincensiusadriel/packer.lua
+++ lua/vincensiusadriel/packer.lua
@@ -70,9 +70,9 @@ return require('packer').startup(function(use)
 
     use { 'fatih/vim-go', { run = ':GoUpdateBinaries' } }
 
-    use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-        require("toggleterm").setup()
-    end }
+    -- use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+    --     require("toggleterm").setup()
+    -- end }
 
     use {
         'numToStr/Comment.nvim',
