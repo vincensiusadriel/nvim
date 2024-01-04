3f6a2c35d38099c62e9c224e46a1c2ef0c103d0b
“vincensiusadrieltoped” <vincensius.adriel@tokopedia.com>
2024-01-04 12:13:27 +0700

    add noice

--- lua/vincensiusadriel/packer.lua
+++ lua/vincensiusadriel/packer.lua
@@ -110,6 +110,21 @@ local plugins = {
     "ThePrimeagen/harpoon",
     "ray-x/lsp_signature.nvim",
 
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
 
 }
 
