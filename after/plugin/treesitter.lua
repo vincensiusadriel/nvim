656e35331d81eb28d22dc93a0c08617362be067b
Vincensius Adriel <vincensius.adriel@bytedance.com>
2025-02-19 17:52:21 +0700

    update treesitter auto update

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,8 +1,26 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "vim", "go", "typescript", "javascript", "c", "cpp", "rust", "lua", "vimdoc" },
+    ensure_installed = {
+        -- "vim", "go", "typescript", "javascript", "c", "cpp", "rust", "lua", "vimdoc"
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "go",
+        "javascript",
+        "json",
+        "lua",
+        "query",
+        "rust",
+        "templ",
+        "thrift",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "vue",
+    },
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
